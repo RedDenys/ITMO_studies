e7937f7cc299e7c8ea54088e346d3c7c5e94869b
RedDenys <124933410+RedDenys@users.noreply.github.com>
2023-04-18 22:35:38 +0300

    Initial commit

---
+++ python_trening/task_1_numbers_1.py
@@ -0,0 +1,16 @@
+a  = 2
+print(a, "относиться к типу", type(a))
+
+b = 15.0001
+print(b, "относиться к типу", type(b))
+
+c = 1+2j
+print(c, "комплексное число ? ", isinstance(c, complex))
+
+print(2+2)
+print(9-5)
+print(7*9)
+print(9/4)
+print(13//2)
+print(13%3)
+print(6**2)
